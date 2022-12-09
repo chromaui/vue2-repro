c2e9c570d24a1a492006cf079b04bd0bb02df2fd
Reuben Ellis <ethriel3695@gmail.com>
2022-12-09 12:06:58 -0700

    Remove play function for now

--- stories/Page.stories.js
+++ stories/Page.stories.js
@@ -1,6 +1,6 @@
-import { within, userEvent } from '@storybook/testing-library';
+import { within, userEvent } from '@storybook/testing-library'
 
-import MyPage from './Page';
+import MyPage from './Page'
 
 export default {
   title: 'Example/Page',
@@ -9,19 +9,19 @@ export default {
     // More on Story layout: https://storybook.js.org/docs/vue/configure/story-layout
     layout: 'fullscreen',
   },
-};
+}
 
 const Template = () => ({
   components: { MyPage },
   template: '<my-page />',
-});
+})
 
-export const LoggedOut = Template.bind({});
+export const LoggedOut = Template.bind({})
 
 // More on interaction testing: https://storybook.js.org/docs/vue/writing-tests/interaction-testing
-export const LoggedIn = Template.bind({});
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
-};
+export const LoggedIn = Template.bind({})
+// LoggedIn.play = async ({ canvasElement }) => {
+//   const canvas = within(canvasElement);
+//   const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+//   await userEvent.click(loginButton);
+// };
